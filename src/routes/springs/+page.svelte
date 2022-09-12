dc64f93bd208aaa66b925ba42be49233c8619b26
Anders Sjöberg <anders.sjoberg@calabrio.com>
2022-09-12 21:05:16 +0200

    Funky slider

--- src/routes/springs/+page.svelte
+++ src/routes/springs/+page.svelte
@@ -1,8 +1,11 @@
 <script>
 	import Bar from '$lib/Bar.svelte';
+	import { onMount } from 'svelte';
 
 	let val = 1;
-	let values = Array(100).fill(10);
+	let numberOfBars = 10;
+
+	$: values = Array(numberOfBars).fill(400);
 
 	/**
 	 * @param {number} min
@@ -13,20 +16,25 @@
 		return val;
 	}
 
-    function randomColor() {
-        return `#${Math.floor(Math.random()*16777215).toString(16)}`;
-    }
+	function randomColor() {
+		return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	}
 
 	function randomBars() {
 		values = values.map(() => random(1, 1000));
 	}
+
+	onMount(() => {
+		randomBars();
+	});
 </script>
 
-<button on:click={randomBars}>Random</button>
+<button on:click={randomBars}>Randomize</button>
+<input type="range" bind:value={numberOfBars} min="5" max="500" on:change={randomBars} />
 <br />
 
 {#each values as value}
-	<Bar {value} width={1000} height={10} color={randomColor()}/>
+	<Bar {value} width={1000} height={1000 / numberOfBars} color={randomColor()} />
 {/each}
 <div />
 

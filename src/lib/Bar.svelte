410493e358bdb6f547c86338d1d6f98933a1682e
Anders Sjöberg <anders.sjoberg@calabrio.com>
2022-09-12 20:36:14 +0200

    Start

---
+++ src/lib/Bar.svelte
@@ -0,0 +1,55 @@
+<script>
+	import { spring } from 'svelte/motion';
+
+    /** @type {number} */
+    export let value;
+
+    /** @type {string} */
+    export let color="#beafff";
+
+    /** @type {number} */
+    export let width=1000;
+    export let height=10;
+
+    export let showText=false
+
+    export let border=false;
+
+	const val = spring(1, { stiffness: 0.3, damping: 0.4 });
+
+	$: $val = value;
+	$: v = $val < 0 ? $val * -1 : $val;
+</script>
+
+<svg viewBox={`0 0 ${width} ${height}`} xmlns="http://www.w3.org/2000/svg" style={`--width:${width}px`}>
+	<g>
+		<rect x="0" y="0" width={v} height={height} fill={color} />
+        {#if border}
+		<rect
+			x="0"
+			y="0"
+			width="1000"
+			height="22"
+			fill="transparent"
+			stroke="#000"
+			stroke-width="1"
+		/>
+        {/if}
+        {#if showText}
+		<text class="small" x="5" y="17">
+			{value}
+		</text>
+        {/if}
+	</g>
+</svg>
+
+<style>
+	svg {
+		width: var(--width);
+	}
+
+	.small {
+		font: sans 10px sans-serif;
+		color: black;
+	}
+</style>
---
+++ src/routes/springs/+page.svelte
@@ -0,0 +1,39 @@
+<script>
+	import Bar from '$lib/Bar.svelte';
+
+	let val = 1;
+	let values = Array(1000).fill(10);
+
+	/**
+	 * @param {number} min
+	 * @param {number} max
+	 */
+	function random(min, max) {
+		val = Math.round(Math.random() * (max - min) + min);
+		return val;
+	}
+
+	function randomBars() {
+		values = values.map(() => random(1, 1000));
+	}
+</script>
+
+<button on:click={randomBars}>Random</button>
+<br />
+
+{#each values as value}
+	<Bar {value} width={1000} height={1} color="#ffaa00"/>
+{/each}
+<div />
+
+<style>
+	button {
+		background-color: #6553b2;
+		color: #f1ffb7;
+		border: none;
+		border-radius: 5px;
+		padding: 10px;
+		font-size: 16px;
+		cursor: pointer;
+	}
+</style>
